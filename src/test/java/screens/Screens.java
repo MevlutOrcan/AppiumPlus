20929d75362b33ec52bb6966e5916c1ed04a0a95
Mevlut Orcan <mevlutorcan@gmail.com>
2022-12-23 09:27:59 +0300

    simdi

---
+++ src/test/java/screens/Screens.java
@@ -0,0 +1,20 @@
+package screens;
+
+import screens.androidScreen.ApiDemosScreen;
+import screens.androidScreen.MainScreen;
+import screens.androidScreen.PreferenceDependencyScreen;
+import screens.androidScreen.PreferenceScreen;
+
+public class Screens {
+    public Screens(){
+
+    }
+    private ApiDemosScreen apiDemosScreen;
+    private MainScreen mainScreen;
+    private PreferenceScreen preferenceScreen;
+    private PreferenceDependencyScreen preferenceDependencyScreen;
+
+
+
+
+}
