92e6457ba5e85ff8eb01ef8eb54b7f50a65e1727
Mevlut Orcan <mevlutorcan@gmail.com>
2022-12-29 20:59:34 +0300

    simdi

--- src/test/java/stepDefinitions/PickerViewStep.java
+++ src/test/java/stepDefinitions/PickerViewStep.java
@@ -3,29 +3,42 @@ package stepDefinitions;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
+import screens.Screens;
+import utils.ReusableMethods;
 
-public class PickerViewStep {
+public class PickerViewStep extends ReusableMethods {
+    Screens screens=new Screens();
     @When("kullanici Picker View tikladi")
     public void kullaniciPickerViewTikladi() {
+        tapOn(screens.alertViewScreen().pickerViewButton);
     }
 
     @Then("kullanici Picker View sayfasinda")
     public void kullaniciPickerViewSayfasinda() {
+        isElementPresent(screens.pickerViewScreen().pickerViewTitle);
     }
 
     @And("kullanici ilk sutuna {string} yazdi")
-    public void kullaniciIlkSutunaYazdi(String arg0) {
+    public void kullaniciIlkSutunaYazdi(String ilk) {
+        screens.pickerViewScreen().firstColumn.sendKeys(ilk);
     }
 
     @And("kullanici ikinci sutuna {string} yazdi")
-    public void kullaniciIkinciSutunaYazdi(String arg0) {
+    public void kullaniciIkinciSutunaYazdi(String ikinci) {
+        screens.pickerViewScreen().secondColumn.sendKeys(ikinci);
     }
 
     @And("kullanici ucuncu sutuna {string} yazdi")
-    public void kullaniciUcuncuSutunaYazdi(String arg0) {
+    public void kullaniciUcuncuSutunaYazdi(String ucuncu) {
+        screens.pickerViewScreen().thirdColumn.sendKeys(ucuncu);
     }
 
     @Then("kullanici yazdigi degerleri {string} {string} {string} gordu")
-    public void kullaniciYazdigiDegerleriGordu(String arg0, String arg1, String arg2) {
+    public void kullaniciYazdigiDegerleriGordu(String ilk, String ikinci, String ucuncu) {
+        Assert.assertEquals(screens.pickerViewScreen().firstColumn.getText(),ilk);
+        Assert.assertEquals(screens.pickerViewScreen().secondColumn.getText(),ikinci);
+        Assert.assertEquals(screens.pickerViewScreen().thirdColumn.getText(),ucuncu);
+
     }
 }
